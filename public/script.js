feb4df1cbd520a6ea4d1158c4df39460a167767f
himanshu <57036053+himanshu-rtn@users.noreply.github.com>
2023-08-25 03:51:02 +0530

    Chat App

---
+++ public/script.js
@@ -0,0 +1,24 @@
+const socket = io();
+
+const messagesContainer = document.querySelector('.messages');
+const messageInput = document.getElementById('message-input');
+const sendButton = document.getElementById('send-button');
+
+sendButton.addEventListener('click', () => {
+    const message = messageInput.value;
+    if (message.trim() !== '') {
+        socket.emit('message', message);
+        messageInput.value = '';
+    }
+});
+
+socket.on('message', (message) => {
+    const messageElement = document.createElement('div');
+    messageElement.innerHTML = `
+    <div class="message sent">
+      <div class="message-content">${message}</div>
+    </div>
+  `;
+    messagesContainer.appendChild(messageElement);
+    messagesContainer.scrollTop = messagesContainer.scrollHeight;
+});
