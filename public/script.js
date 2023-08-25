b122d6e92059743c394015eaa037b806c64f92aa
himanshu <r99.himanshu@gmail.com>
2023-08-25 20:20:33 +0530

    new id

--- public/script.js
+++ public/script.js
@@ -5,20 +5,20 @@ const messageInput = document.getElementById('message-input');
 const sendButton = document.getElementById('send-button');
 
 sendButton.addEventListener('click', () => {
-    const message = messageInput.value;
-    if (message.trim() !== '') {
-        socket.emit('message', message);
-        messageInput.value = '';
-    }
+  const message = messageInput.value;
+  if (message.trim() !== '') {
+    socket.emit('message', message);
+    messageInput.value = '';
+  }
 });
 
 socket.on('message', (message) => {
-    const messageElement = document.createElement('div');
-    messageElement.innerHTML = `
+  const messageElement = document.createElement('div');
+  messageElement.innerHTML = `
     <div class="message sent">
       <div class="message-content">${message}</div>
     </div>
   `;
-    messagesContainer.appendChild(messageElement);
-    messagesContainer.scrollTop = messagesContainer.scrollHeight;
+  messagesContainer.appendChild(messageElement);
+  messagesContainer.scrollTop = messagesContainer.scrollHeight;
 });
