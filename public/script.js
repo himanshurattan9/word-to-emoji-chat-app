5b07fb7df388c0dde7ff0de968c6d4bdcf3214df
himanshu <r99.himanshu@gmail.com>
2023-08-27 02:30:48 +0530

    Emoji Functionality Added

--- public/script.js
+++ public/script.js
@@ -4,10 +4,39 @@ const messagesContainer = document.querySelector('.messages');
 const messageInput = document.getElementById('message-input');
 const sendButton = document.getElementById('send-button');
 
+function wordToEmoji(word) {
+  const emojiMap = {
+    "happy": "😄",
+    "sad": "😢",
+    "love": "❤️",
+    "angry": "😡",
+    "laugh": "😂",
+    "cool": "😎",
+    "cry": "😭",
+    "confused": "😕",
+    "heart": "💖",
+    "star": "⭐"
+  };
+
+  const lowerCaseWord = word.toLowerCase();
+  if (emojiMap.hasOwnProperty(lowerCaseWord)) {
+    return emojiMap[lowerCaseWord];
+  } else {
+    return word; // Return the original word if no matching emoji found
+  }
+}
+
+function convertSentenceToEmojis(sentence) {
+  const words = sentence.split(' ');
+  const convertedWords = words.map(word => wordToEmoji(word));
+  return convertedWords.join(' ');
+}
+
 sendButton.addEventListener('click', () => {
   const message = messageInput.value;
   if (message.trim() !== '') {
-    socket.emit('message', message);
+    const emojiMessage = convertSentenceToEmojis(message); // Convert matching words to emojis
+    socket.emit('message', emojiMessage);
     messageInput.value = '';
   }
 });
